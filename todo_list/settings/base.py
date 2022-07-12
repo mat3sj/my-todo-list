ace9cc685ebac6884b40233e2935a1cf6cdb169f
mat3sj <mat3s.j@gmail.com>
2022-07-12 09:37:49 +0200

    creating master branch

--- todo_list/settings/base.py
+++ todo_list/settings/base.py
@@ -5,9 +5,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l6_96kw5r-au1$c^o9-x_l0-!(m)1t74s88xm91ygom*^xu@e0'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
